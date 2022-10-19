6dc5db19f68b41d2156c82784742e41e87ff6fad
Jonatas Martins <107153976+jonatas-martins@users.noreply.github.com>
2022-10-19 12:20:17 -0300

    feat: creating function setCard

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
 import "./css/index.css"
+
+const ccBgColorRight = document.querySelector(".cc__bg-right")
+const ccBgColorLeft = document.querySelector(".cc__bg-left")
+const ccLogo = document.querySelector(".cc-logo-img")
+
+function setCard(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    master: ["#C69347", "#DF6F29"],
+    default: ["black", "gray"],
+  }
+
+  const logos = {
+    visa: ["/cc-visa.svg"],
+    master: ["/cc-mastercard.svg"],
+    default: ["/cc-default.svg"],
+  }
+
+  ccBgColorRight.setAttribute("fill", colors[type][0])
+  ccBgColorLeft.setAttribute("fill", colors[type][1])
+
+  ccLogo.setAttribute("src", logos[type])
+}
+
+setCard("default")
+globalThis.setCard = setCard // transformando em função global
