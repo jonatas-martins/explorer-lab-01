e626f331b0d8e79036617f468a5fa694a310564a
Jonatas Martins <107153976+jonatas-martins@users.noreply.github.com>
2022-10-20 15:22:08 -0300

    feat: add imask

--- src/main.js
+++ src/main.js
@@ -1,8 +1,49 @@
 import "./css/index.css"
+import IMask from "imask"
 
 const ccBgColorRight = document.querySelector(".cc__bg-right")
 const ccBgColorLeft = document.querySelector(".cc__bg-left")
 const ccLogo = document.querySelector(".cc-logo-img")
+
+const inputNumberCard = document.getElementById("card-number")
+const inputNumberCardPattern = {
+  mask: "0000 0000 0000 0000",
+}
+const inputNumberCardMasked = IMask(inputNumberCard, inputNumberCardPattern)
+
+const inputCardExpiration = document.getElementById("expiration-date")
+const inputCardExpirationPattern = {
+  mask: "MM{/}YYYY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: new Date().getFullYear(),
+      to: new Date().getFullYear() + 10,
+      minLength: 4,
+    },
+
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+      minLength: 2,
+    },
+  },
+}
+const inputCardExpirationMasked = IMask(
+  inputCardExpiration,
+  inputCardExpirationPattern
+)
+
+const inputSecurityCode = document.getElementById("security-code")
+const inputSecurityCodePattern = {
+  mask: "0000",
+  minLength: 3,
+}
+const inputSecurityCodeMasked = IMask(
+  inputSecurityCode,
+  inputSecurityCodePattern
+)
 
 function setCard(type) {
   const colors = {
@@ -23,5 +64,5 @@ function setCard(type) {
   ccLogo.setAttribute("src", logos[type])
 }
 
-setCard("default")
+setCard("visa")
 globalThis.setCard = setCard // transformando em função global
